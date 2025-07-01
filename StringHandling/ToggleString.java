7cf71849affc78b4bf9041bd0997e9814baa4d23
AmritanshuGupta <amritanshu.gupta_cs22@gla.ac.in>
2025-07-01 10:18:52 +0530

    Added : Program to Toggle an input string i.e. Convert lowercase to uppercase and vice-versa

---
+++ StringHandling/ToggleString.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+// Toggle an input string i.e. Convert lowercase to uppercase and vice-versa
+public class ToggleString 
+{
+    public static String toggle(String input)
+    {
+        String str="";
+        for(int i=0; i<input.length(); i++)
+	{
+            char ch=input.charAt(i);
+            if(Character.isUpperCase(ch))
+	    {
+                str+=Character.toLowerCase(ch);
+            }
+            else
+                str+=Character.toUpperCase(ch);
+        }
+        return str;   
+    }
+
+    public static void main(String... args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        System.out.println("The Toggle string of "+input+" is " + toggle(input));
+    }
+}
