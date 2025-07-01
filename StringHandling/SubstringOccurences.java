667cd49c56b475f807f21994b9a5d95f98e8d629
AmritanshuGupta <amritanshu.gupta_cs22@gla.ac.in>
2025-07-01 10:17:19 +0530

    Added : Program to Count occurences of a given a sub string in a string

---
+++ StringHandling/SubstringOccurences.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+// Count occurences of a given a sub string in a string
+public class SubstringOccurences 
+{
+    public static int countOccurrence(String str, String substring)
+    {
+        int count = 0;
+        int index = 0;
+
+        while ((index = str.indexOf(substring, index)) != -1) 
+	{
+            count++;
+            index += substring.length(); // Move past the found substring
+        }
+
+        return count;
+    }
+
+    public static void main(String... args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.print("Enter a substring: ");
+        String substring = sc.nextLine();
+        System.out.println("Occurrence of the substring in the string is: " + countOccurrence(str, substring));
+    }
+}
