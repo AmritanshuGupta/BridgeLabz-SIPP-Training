b5fd2f0d12f573869f46ebb546a176e61b3bbe90
AmritanshuGupta <amritanshu.gupta_cs22@gla.ac.in>
2025-07-01 10:15:01 +0530

    Added : Program to replace old word with a new one

---
+++ StringHandling/ReplaceOldWithNew.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+// Replace old word with a new one
+public class ReplaceOldWithNew 
+{
+
+    public static String replaceWord(String sentence, String oldWord, String newWord) 
+    {
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) 
+	{
+            if (words[i].equals(oldWord))
+	    {
+                result += newWord;
+            }
+	    else
+	    {
+                result += words[i];
+            }
+
+            if (i != words.length - 1) 
+	    {
+                result += " ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String... args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        System.out.print("Enter the word to replace: ");
+        String oldWord = sc.next();
+
+        System.out.print("Enter the new word: ");
+        String newWord = sc.next();
+
+        String updatedSentence = replaceWord(sentence, oldWord, newWord);
+        System.out.println("Updated sentence: " + updatedSentence);
+    }
+}
